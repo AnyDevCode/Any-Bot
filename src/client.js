5df88d5fd4a64c70b49cd2bad2e7f2033e0f50c2
MDC <mdc@mycommand.xyz>
2022-02-03 20:08:47 -0500

    feat: Support Server Compatibility
    Now you can put the support server links natively.

--- src/client.js
+++ src/client.js
@@ -11,239 +11,239 @@ let queue = new Map()
  */
 class Client extends Discord.Client {
 
-  /**
-   * Create a new client
-   * @param {Object} config
-   * @param {ClientOptions} options
-   */
-  constructor(config, options = {}) {
+    /**
+     * Create a new client
+     * @param {Object} config
+     * @param {{ws: {intents}}} options
+     */
+    constructor(config, options = {}) {
 
-    super(options);
+        super(options);
+
+        /**
+         * Create logger
+         */
+        this.logger = require('./utils/logger.js');
+
+        /**
+         * Create database
+         */
+        this.db = require('./utils/db.js');
+
+        /**
+         * All possible command types
+         * @type {Object}
+         */
+        this.types = {
+            INFO: 'info',
+            FUN: 'fun',
+            ANIMALS: 'animals',
+            COLOR: 'color',
+            POINTS: 'points',
+            LEVELS: 'levels',
+            MISC: 'misc',
+            GAMES: 'games',
+            SOCIAL: 'social',
+            MOD: 'mod',
+            ADMIN: 'admin',
+            MUSIC: 'music',
+            BACKUP: 'backup',
+            OWNER: 'owner',
+            NSFW: 'nsfw'
+        };
+
+        /**
+         * Collection of bot commands
+         * @type {Collection<string, Command>}
+         */
+        this.commands = new Discord.Collection();
+
+        /**
+         * Collection of command aliases
+         * @type {Collection<string, Command>}
+         */
+        this.aliases = new Discord.Collection();
+
+        /**
+         * Array of trivia topics
+         * @type {Array<string>}
+         */
+        this.topics = [];
+
+        /**
+         * Login token
+         * @type {string}
+         */
+        this.token = config.token;
+
+        /**
+         * API keys
+         * @type {Object}
+         */
+        this.apiKeys = config.apiKeys;
+
+        /**
+         * Any Bot's owner ID
+         * @type {string}
+         */
+        //Check if the owner ID in the config is a number
+        this.ownerID = config.ownerId
+
+        /**
+         * Any Bot's bug report channel ID
+         * @type {string}
+         */
+        this.bugReportChannelId = config.bugReportChannelId;
+
+        /**
+         * Any Bot's feedback channel ID
+         * @type {string}
+         */
+        this.feedbackChannelId = config.feedbackChannelId;
+
+        /**
+         * Any Bot's server log channel ID
+         * @type {string}
+         */
+        this.serverLogId = config.serverLogId;
+
+        /**
+         * Any Bot's Support Server Invite
+         * @type {string}
+         */
+        this.supportServerInvite = config.supportServerInvite;
+
+        /**
+         * Any Bot's Version
+         * @type {string}
+         */
+        let version = require('../package.json').version;
+        this.version = version;
+
+        /**
+         * Utility functions
+         * @type {Object}
+         */
+        this.utils = require('./utils/utils.js');
+
+        this.logger.info('Initializing...');
+
+    }
 
     /**
-     * Create logger
+     * Loads all available events
+     * @param {string} path
      */
-    this.logger = require('./utils/logger.js');
-
-    /**
-     * Create database
-     */
-    this.db = require('./utils/db.js');
-
-    /**
-     * All possible command types
-     * @type {Object}
-     */
-    this.types = {
-      INFO: 'info',
-      FUN: 'fun',
-      ANIMALS: 'animals',
-      COLOR: 'color',
-      POINTS: 'points',
-      LEVELS: 'levels',
-      MISC: 'misc',
-      GAMES: 'games',
-      SOCIAL: 'social',
-      MOD: 'mod',
-      ADMIN: 'admin',
-      MUSIC: 'music',
-      BACKUP: 'backup',
-      OWNER: 'owner',
-      NSFW: 'nsfw'
-    };
-
-    /**
-     * Collection of bot commands
-     * @type {Collection<string, Command>}
-     */
-    this.commands = new Discord.Collection();
-
-    /**
-     * Collection of command aliases
-     * @type {Collection<string, Command>}
-     */
-    this.aliases = new Discord.Collection();
-
-    /**
-     * Collection of command cooldowns
-     * @type {Collection<string, number>}
-     */
-    this.cooldowns = new Discord.Collection();
-
-    /**
-     * Array of trivia topics
-     * @type {Array<string>}
-     */
-    this.topics = [];
-
-    /**
-     * Login token
-     * @type {string}
-     */
-    this.token = config.token;
-
-    /**
-     * API keys
-     * @type {Object}
-     */
-    this.apiKeys = config.apiKeys;
-
-    /**
-     * Any Bot's owner ID
-     * @type {string}
-     */
-    this.ownerId = config.ownerId;
-
-    /**
-     * Any Bot's bug report channel ID
-     * @type {string}
-     */
-    this.bugReportChannelId = config.bugReportChannelId;
-
-    /**
-     * Any Bot's feedback channel ID
-     * @type {string}
-     */
-    this.feedbackChannelId = config.feedbackChannelId;
-
-    /**
-     * Any Bot's server log channel ID
-     * @type {string}
-     */
-    this.serverLogId = config.serverLogId;
-
-    /**
-     * Utility functions
-     * @type {Object}
-     */
-    this.utils = require('./utils/utils.js');
-
-    this.logger.info('Initializing...');
-
-  }
-
-  /**
-   * Loads all available events
-   * @param {string} path
-   */
-  loadEvents(path) {
-    readdir(path, (err, files) => {
-      if (err) this.logger.error(err);
-      files = files.filter(f => f.split('.').pop() === 'js');
-      if (files.length === 0) return this.logger.warn('No events found');
-      this.logger.info(`${files.length} event(s) found...`);
-      files.forEach(f => {
-        const eventName = f.substring(0, f.indexOf('.'));
-        const event = require(resolve(__basedir, join(path, f)));
-        super.on(eventName, event.bind(null, this));
-        delete require.cache[require.resolve(resolve(__basedir, join(path, f)))]; // Clear cache
-        this.logger.info(`Loading event: ${eventName}`);
-      });
-    });
-    return this;
-  }
-
-  /**
-   * Loads all available commands
-   * @param {string} path
-   */
-  loadCommands(path) {
-    this.logger.info('Loading commands...');
-    let table = new AsciiTable('Commands');
-    table.setHeading('File', 'Aliases',"Cooldown", 'Type', 'Status');
-    readdirSync(path).filter( f => !f.endsWith('.js')).forEach( dir => {
-      const commands = readdirSync(resolve(__basedir, join(path, dir))).filter(f => f.endsWith('js'));
-      commands.forEach(f => {
-        const Command = require(resolve(__basedir, join(path, dir, f)));
-        const command = new Command(this); // Instantiate the specific command
-        if (command.name && !command.disabled) {
-          // Map command
-          this.commands.set(command.name, command);
-          // Map command aliases
-          let aliases = '';
-          if (command.aliases) {
-            command.aliases.forEach(alias => {
-              this.aliases.set(alias, command);
+    loadEvents(path) {
+        readdir(path, (err, files) => {
+            if (err) this.logger.error(err);
+            files = files.filter(f => f.split('.').pop() === 'js');
+            if (files.length === 0) return this.logger.warn('No events found');
+            this.logger.info(`${files.length} event(s) found...`);
+            files.forEach(f => {
+                const eventName = f.substring(0, f.indexOf('.'));
+                const event = require(resolve(__basedir, join(path, f)));
+                super.on(eventName, event.bind(null, this));
+                delete require.cache[require.resolve(resolve(__basedir, join(path, f)))]; // Clear cache
+                this.logger.info(`Loading event: ${eventName}`);
             });
-            aliases = command.aliases.join(', ');
-          }
-          // Map command cooldowns
-          let cooldowns = '';
-          if (command.cooldowns) {
-            command.cooldowns.forEach(cooldown => {
-              this.cooldowns.set(cooldown, command);
+        });
+        return this;
+    }
+
+    /**
+     * Loads all available commands
+     * @param {string} path
+     */
+    loadCommands(path) {
+        this.logger.info('Loading commands...');
+        let table = new AsciiTable('Commands');
+        table.setHeading('File', 'Aliases',"Cooldown", 'Type', 'Status');
+        readdirSync(path).filter( f => !f.endsWith('.js')).forEach( dir => {
+            const commands = readdirSync(resolve(__basedir, join(path, dir))).filter(f => f.endsWith('js'));
+            commands.forEach(f => {
+                const Command = require(resolve(__basedir, join(path, dir, f)));
+                const command = new Command(this); // Instantiate the specific command
+                if (command.name && !command.disabled) {
+                    // Map command
+                    this.commands.set(command.name, command);
+                    // Map command aliases
+                    let aliases = '';
+                    if (command.aliases) {
+                        command.aliases.forEach(alias => {
+                            this.aliases.set(alias, command);
+                        });
+                        aliases = command.aliases.join(', ');
+                    }
+                    table.addRow(f, aliases, command.type, 'pass');
+                } else {
+                    this.logger.warn(`${f} failed to load`);
+                    table.addRow(f, '', '', 'fail');
+                }
             });
-            cooldowns = command.cooldowns.join(', ');
-          }
-          table.addRow(f, aliases, cooldowns, command.type, 'pass');
-        } else {
-          this.logger.warn(`${f} failed to load`);
-          table.addRow(f, '', '', 'fail');
-        }
-      });
-    });
-    this.logger.info(`\n${table.toString()}`);
-    return this;
-  }
+        });
+        this.logger.info(`\n${table.toString()}`);
+        return this;
+    }
 
-  /**
-   * Loads all available trivia topics
-   * @param {string} path
-   */
-  loadTopics(path) {
-    readdir(path, (err, files) => {
-      if (err) this.logger.error(err);
-      files = files.filter(f => f.split('.').pop() === 'yml');
-      if (files.length === 0) return this.logger.warn('No topics found');
-      this.logger.info(`${files.length} topic(s) found...`);
-      files.forEach(f => {
-        const topic = f.substring(0, f.indexOf('.'));
-        this.topics.push(topic);
-        this.logger.info(`Loading topic: ${topic}`);
-      });
-    });
-    return this;
-  }
+    /**
+     * Loads all available trivia topics
+     * @param {string} path
+     */
+    loadTopics(path) {
+        readdir(path, (err, files) => {
+            if (err) this.logger.error(err);
+            files = files.filter(f => f.split('.').pop() === 'yml');
+            if (files.length === 0) return this.logger.warn('No topics found');
+            this.logger.info(`${files.length} topic(s) found...`);
+            files.forEach(f => {
+                const topic = f.substring(0, f.indexOf('.'));
+                this.topics.push(topic);
+                this.logger.info(`Loading topic: ${topic}`);
+            });
+        });
+        return this;
+    }
 
-  /**
-   * Checks if user is the bot owner
-   * @param {User} user
-   */
-  isOwner(user) {
-    return user.id === this.ownerId;
-  }
+    /**
+     * Checks if user is the bot owner
+     * @param {User} user
+     */
+    isOwner(user) {
+        return parseInt(user.id) === parseInt(this.ownerID);
+    }
 
-  queue(){ 
-    return queue
-  }
+    queue(){
+        return queue
+    }
 
 
-  /**
-   * Creates and sends system failure embed
-   * @param {Guild} guild
-   * @param {string} error
-   * @param {string} errorMessage
-   */
-  sendSystemErrorMessage(guild, error, errorMessage) {
+    /**
+     * Creates and sends system failure embed
+     * @param {Guild} guild
+     * @param {string} error
+     * @param {string} errorMessage
+     */
+    sendSystemErrorMessage(guild, error, errorMessage) {
 
-    // Get system channel
-    const systemChannelId = this.db.settings.selectSystemChannelId.pluck().get(guild.id);
-    const systemChannel = guild.channels.cache.get(systemChannelId);
+        // Get system channel
+        const systemChannelId = this.db.settings.selectSystemChannelId.pluck().get(guild.id);
+        const systemChannel = guild.channels.cache.get(systemChannelId);
 
-    if ( // Check channel and permissions
-      !systemChannel ||
-      !systemChannel.viewable ||
-      !systemChannel.permissionsFor(guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS'])
-    ) return;
+        if ( // Check channel and permissions
+            !systemChannel ||
+            !systemChannel.viewable ||
+            !systemChannel.permissionsFor(guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS'])
+        ) return;
 
-    const embed = new Discord.MessageEmbed()
-      .setAuthor(`${this.user.tag}`, this.user.displayAvatarURL({ dynamic: true }))
-      .setTitle(`${fail} System Error: \`${error}\``)
-      .setDescription(`\`\`\`diff\n- System Failure\n+ ${errorMessage}\`\`\``)
-      .setTimestamp()
-      .setColor(guild.me.displayHexColor);
-    systemChannel.send(embed);
-  }
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`${this.user.tag}`, this.user.displayAvatarURL({ dynamic: true }))
+            .setTitle(`${fail} System Error: \`${error}\``)
+            .setDescription(`\`\`\`diff\n- System Failure\n+ ${errorMessage}\`\`\``)
+            .setTimestamp()
+            .setColor(guild.me.displayHexColor);
+        systemChannel.send(embed);
+    }
 }
 
 module.exports = Client;

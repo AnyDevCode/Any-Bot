7979d314200ebaac7cac6911ac526f0992a767bc
MDC <mdc@mycommand.xyz>
2022-02-24 23:51:09 -0500

    fix: GD Api Down

--- src/commands/fun/geometrydashsay.js
+++ src/commands/fun/geometrydashsay.js
@@ -18,12 +18,9 @@ module.exports = class GeometryDashsayCommand extends Command {
     
     let char = ["gatekeeper", "gatekeeper.dark", "keymaster", "keymaster.huh", "keymaster.scared", "keymaster.scream", "monster", "monster.eyes", "potbor", "potbor.annoyed", "potbor.huh", "potbor.mad", "potbor.right", "potbor.talk", "potbor.tired", "scratch", "scratch.annoyed", "scratch.huh", "scratch.mad", "scratch.right", "scratch.talk", "shopkeeper", "shopkeeper.annoyed", "spooky"]   
     let color = ["blue", "brown", "purple", "aqua", "green", "grey", "orange", "pink", "red"] 
-    let mod = ["mod", "elder"] 
-        
+
     let capture = char[Math.floor(char.length * Math.random())];  
     let colorize = color[Math.floor(color.length * Math.random())];
-    let moder = mod[Math.floor(mod.length * Math.random())];  
-    
     
     let autor = stringToUrlEncoded(message.author.username)
         
@@ -31,9 +28,9 @@ module.exports = class GeometryDashsayCommand extends Command {
         
     if (!txt) return message.channel.send("Do not forget to put the text you want.") 
 
-    let links = [`https://gdcolon.com/tools/gdtextbox/img/${txt}?color=${colorize}&name=${autor}&char=${capture}`, `https://gdcolon.com/tools/gdcomment/img/${txt}?name=${autor}&likes=${Math.floor(500 * Math.random())}&%=${Math.floor(100 * Math.random())}&mod=${moder}&customIcon=${autor}` ]
+    let links = [`https://gdcolon.com/tools/gdtextbox/img/${txt}?color=${colorize}&name=${autor}&char=${capture}`]
         
-    let image = links[Math.floor(links.length * Math.random())]; 
+    let image = links[0];
     
     let attachment = new Discord.MessageAttachment(image, "GD.png");
     

86bc3dd144b6a35041fcf0845b13674fa103185e
MDC <mdc@mycommand.xyz>
2022-02-24 22:10:46 -0500

    refactor: Server Icon in Best Quality

--- src/commands/info/servericon.js
+++ src/commands/info/servericon.js
@@ -14,7 +14,7 @@ module.exports = class ServerIconCommand extends Command {
   run(message) {
     const embed = new MessageEmbed()
       .setTitle(`${message.guild.name}'s Icon`)
-      .setImage(message.guild.iconURL({ dynamic: true, size: 512 }))
+      .setImage(message.guild.iconURL({ dynamic: true, size: 2048 }))
       .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setColor(message.guild.me.displayHexColor);

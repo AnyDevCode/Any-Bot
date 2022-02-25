5def584b6695253c25a109bfdf93b506cdc79088
MDC <mdc@mycommand.xyz>
2022-02-24 21:47:42 -0500

    fix: New NPM for the chatbot

--- src/commands/fun/ChatBot.js
+++ src/commands/fun/ChatBot.js
@@ -1,6 +1,6 @@
 // Dependencies:
 const {MessageEmbed} = require('discord.js');
-const fetch = require('node-fetch');
+const cleverbot = require("cleverbot-free");
 // Command Require:
 const Command = require('../Command.js');
 
@@ -9,7 +9,7 @@ module.exports = class ChatBotCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'chatbot',
-            aliases: ['chb', 'cbot'],
+            aliases: ['chb', 'cbot', 'brain'],
             usage: 'chatbot <message>',
             description: 'He answers what you tell him.',
             cooldown: 5,
@@ -20,35 +20,25 @@ module.exports = class ChatBotCommand extends Command {
     // Command Code:
     async run(message, args) {
 
-        // String to URL encode:
-        const {stringToUrlEncoded} = message.client.utils;
 
         // Check if it has args:
         if (!args[0]) return this.sendErrorMessage(message, 0, 'Put some text that you want me to answer you');
         // Args:
         let text = args.join(" ");
         // Try to fetch the answer:
-        try {
-            // Get the Api Key:
-            const apiKey = message.client.apiKeys.somerandomapikey
-            // Fetch the answer:
-            const res = await fetch(`https://some-random-api.ml/chatbot?message=${stringToUrlEncoded(text)}&key=${apiKey}`);
-            // Get the answer:
-            const answer = (await res.json());
-            // Create the embed:
-            const embed = new MessageEmbed()
-                .setTitle(':robot:  Chat Bot!  :robot:')
-                .setDescription(answer.response)
-                .setFooter(message.member.displayName, message.author.displayAvatarURL({dynamic: true}))
-                .setTimestamp()
-                .setColor(message.guild.me.displayHexColor);
-            // Check if the answer is valid:
-            if (answer.error) return embed.setDescription("I'm sorry, I can't answer that right now. Please try again later.");
-            // Send the embed:
-            message.channel.send(embed);
-        } catch (err) {
+
+            await cleverbot(text).then(response => {
+                const embed = new MessageEmbed()
+                    .setTitle(':robot:  Chat Bot!  :robot:')
+                    .setDescription(response)
+                    .setFooter(message.member.displayName, message.author.displayAvatarURL({dynamic: true}))
+                    .setTimestamp()
+                    .setColor(message.guild.me.displayHexColor);
+                return message.channel.send(embed);
+            }).catch(() => {
             // Send the error message:
-            message.client.sendErrorMessage(message, 1, "I'm sorry, I can't answer that right now. Please try again later.");
-        }
+            this.sendErrorMessage(message, 1, "I'm sorry, I can't answer that right now. Please try again later.");
+        })
+
     }
 };

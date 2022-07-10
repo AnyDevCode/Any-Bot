c45f574e73f7a2125d72c92e3487d62b088f8070
MDCDEV <me@mdcdev.me>
2022-07-09 22:14:08 -0500

    fix!: Yeah, a bug, yessssss

--- src/client.js
+++ src/client.js
@@ -33,11 +33,6 @@ class Client extends Discord.Client {
     this.logger = require("./utils/logger.js");
 
     /**
-     * Create database
-     */
-    this.db = require("./utils/db.js");
-
-    /**
      * Create MongoDB
      */
     this.mongodb = require("./utils/mongodb.js");

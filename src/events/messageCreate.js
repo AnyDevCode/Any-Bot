926a70bdb5b65a999cfa6e33e09f27877acc7465
MDCDEV <me@mdcdev.me>
2022-07-25 12:40:29 -0500

    feat: New AntiPhishing Links System

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,34 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
-const { oneLine, stripIndents } = require("common-tags");
+const {
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton
+} = require("discord.js");
+const {
+  oneLine,
+  stripIndents
+} = require("common-tags");
+const fs = require("fs");
+const {
+  join
+} = require("path");
+
+let phisingLinks = null;
+let cache_date;
+
+function update_cache() {
+  if (phisingLinks === null || Date.now() - cache_date > (1000 * 60 * 60)) {
+    fs.readFile(join(__basedir, "data/PhishingLinks.json"), (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      phisingLinks = JSON.parse(data);
+    })
+    cache_date = Date.now();
+  }
+}
+
+update_cache();
 
 module.exports = {
   name: "messageCreate",
@@ -8,23 +37,23 @@ module.exports = {
       return message.channel.send({
         embeds: [
           new MessageEmbed()
-            .setColor("#0099ff")
-            .setTitle(`Hi, i am ${client.user.username}`)
-            .setDescription(
-              stripIndents`
+          .setColor("#0099ff")
+          .setTitle(`Hi, i am ${client.user.username}`)
+          .setDescription(
+            stripIndents `
             Please, use me in a server.
             If you want to know more about me, use the command \`@${client.user.username} help\` in a server.
             For more information about me, check out my [GitHub](https://github.com/MDCYT/Any-Bot).
             For Support and help, join the [Support Server](https://discord.gg/5UyuwbNu8j).
             For Terms and Conditions, visit the [Terms and Conditions](https://any-bot.tech/tos).
             `
-            )
-            .setFooter({
-              text: `${client.user.username}`,
-              iconURL: client.user.avatarURL(),
-            })
-            .setThumbnail(client.user.avatarURL())
-            .setTimestamp(),
+          )
+          .setFooter({
+            text: `${client.user.username}`,
+            iconURL: client.user.avatarURL(),
+          })
+          .setThumbnail(client.user.avatarURL())
+          .setTimestamp(),
         ],
       });
     if (!message.channel.viewable || message.author.bot) return;
@@ -54,6 +83,9 @@ module.exports = {
       disabledCommands: disabledCommands,
       prefix: prefix,
       modChannelIDs: modChannelIds,
+      antiPhishing: antiPhishing,
+      antiPhishingLogsChannelID: antiPhishingLogsChannelId,
+      antiPhishingSystem: antiPhishingSystem,
     } = await client.mongodb.settings.selectRow(message.guild.id);
 
     if (typeof disabledCommands !== "string") disabledCommands = [];
@@ -64,7 +96,108 @@ module.exports = {
     if (typeof modChannelIds === "string")
       modChannelIds = modChannelIds.split(" ");
 
-    let { level, xp } = await client.mongodb.users.selectRow(
+    if (antiPhishing) {
+      update_cache()
+
+      // Match urls only
+      // Example: https://discordapp.com/test/
+      // Group 1: domain + path (discordapp.com/test)
+      // Group 2: domain (discordapp.com)
+      // Group 3: path (/test)
+      const regex = /(?:(?:https?|ftp|mailto):\/\/)?(?:www\.)?(([^\/\s]+\.[a-z\.]+)(\/[^\s]*)?)(?:\/)?/ig;
+
+      let susDomainsArgs = [];
+
+      // Extract all the matched urls
+      const theMessage = message.content;
+      for (let match of theMessage.matchAll(regex)) {
+        susDomainsArgs.push(match[2]);
+      }
+
+      // Check if the message contains a phishing link
+      // PhishingLinks have a property called "domains", which is an array of domains
+      // If the message contains a phishing link, the bot will send a message to the mod channel
+
+      let phishingLink = false;
+
+      for (let PhishingLink of phisingLinks.domains) {
+        for (let domain of susDomainsArgs) {
+          if (PhishingLink === domain) {
+            phishingLink = true;
+            break;
+          }
+        }
+      }
+
+      if (phishingLink) {
+        const antiPhishingLogsChannel = message.guild.channels.cache.get(antiPhishingLogsChannelId);
+        if (antiPhishingLogsChannel) {
+
+          antiPhishingLogsChannel.send({
+            embeds: [
+              new MessageEmbed()
+              .setTitle("Message Update: `Phishing Link Detected`")
+              .setDescription(message.content)
+              .setAuthor({
+                name: message.author.tag,
+                iconURL: message.author.displayAvatarURL({
+                  dynamic: true
+                }),
+              })
+              .setTimestamp()
+              .setColor(message.guild.me.displayHexColor)
+              .setFooter({
+                text: `${client.user.username}`,
+                iconURL: client.user.avatarURL(),
+              })
+
+            ],
+          });
+        }
+
+
+        switch (antiPhishingSystem) {
+          case 0:
+            break
+          case 1:
+            message.delete();
+            break
+          case 2:
+            message.delete();
+            //TimeOut the user for 1 week
+            if (message.member.moderatable && message.member.manageable) message.member.timeout(1000 * 60 * 60 * 7, "Phishing Link - AutoMod")
+            else message.channel.send({
+              content: "The user " + message.author.tag + " is sending a phishing, please report this to the server owner or a mod.",
+            })
+            break
+          case 3:
+            message.delete();
+            //Kick the user
+            if (message.member.kickable && message.member.moderatable && message.member.manageable) message.member.kick("Phishing Link - AutoMod")
+            else message.channel.send({
+              content: "The user " + message.author.tag + " is sending a phishing, please report this to the server owner or a mod.",
+            })
+            break
+          case 4:
+            message.delete();
+            //Ban the user
+            if (message.member.bannable && message.member.moderatable && message.member.manageable) message.member.ban("Phishing Link - AutoMod")
+            else message.channel.send({
+              content: "The user " + message.author.tag + " is sending a phishing, please report this to the server owner or a mod.",
+            })
+            break
+          default:
+            break
+        }
+
+      }
+
+    }
+
+    let {
+      level,
+      xp
+    } = await client.mongodb.users.selectRow(
       message.author.id,
       message.guild.id
     );
@@ -109,8 +242,8 @@ module.exports = {
             command.type !== client.types.MOD ||
             (command.type === client.types.MOD &&
               message.channel
-                .permissionsFor(message.author)
-                .missing(command.userPermissions) !== 0)
+              .permissionsFor(message.author)
+              .missing(command.userPermissions) !== 0)
           )
             return; // Return early so Any Bot doesn't respond
         }
@@ -142,8 +275,7 @@ module.exports = {
 
           await client.mongodb.users.updateLevelXPPointsCommandsandMessages(
             message.author.id,
-            message.guild.id,
-            {
+            message.guild.id, {
               level: newLevel || 0,
               xp: NewXP || 0,
               points: NewPoints || 0,
@@ -159,8 +291,8 @@ module.exports = {
         (message.content === `<@${client.user.id}>` ||
           message.content === `<@!${client.user.id}>`) &&
         message.channel
-          .permissionsFor(message.guild.me)
-          .has(["SEND_MESSAGES", "EMBED_LINKS"]) &&
+        .permissionsFor(message.guild.me)
+        .has(["SEND_MESSAGES", "EMBED_LINKS"]) &&
         !modChannelIds.includes(message.channel.id)
       ) {
         const embed = new MessageEmbed()
@@ -171,14 +303,14 @@ module.exports = {
           )
           .addField(
             "Invite Me",
-            oneLine`
+            oneLine `
             You can add me to your server by clicking 
             [here](https://discordapp.com/oauth2/authorize?client_id=${message.guild.me.id}&scope=applications.commands%20bot&permissions=8)
           `
           )
           .addField(
             "Support",
-            oneLine`
+            oneLine `
             If you have questions, suggestions, or found a bug, please join the 
             [${message.guild.me.displayName} Support Server](${message.client.supportServerInvite})
           `
@@ -188,19 +320,19 @@ module.exports = {
         const linkrow = new MessageActionRow()
           .addComponents(
             new MessageButton()
-              .setLabel("Invite Me")
-              .setStyle("LINK")
-              .setURL(
-                `https://discordapp.com/oauth2/authorize?client_id=${message.guild.me.id}&scope=applications.commands%20bot&permissions=8`
-              )
-              .setEmoji("➡")
+            .setLabel("Invite Me")
+            .setStyle("LINK")
+            .setURL(
+              `https://discordapp.com/oauth2/authorize?client_id=${message.guild.me.id}&scope=applications.commands%20bot&permissions=8`
+            )
+            .setEmoji("➡")
           )
           .addComponents(
             new MessageButton()
-              .setLabel("Support Server")
-              .setStyle("LINK")
-              .setURL(message.client.supportServerInvite)
-              .setEmoji("🛠️")
+            .setLabel("Support Server")
+            .setStyle("LINK")
+            .setURL(message.client.supportServerInvite)
+            .setEmoji("🛠️")
           );
 
         message.channel.send({
@@ -234,8 +366,7 @@ module.exports = {
     // Update the total messages
     await client.mongodb.users.updateLevelXPPointsCommandsandMessages(
       message.author.id,
-      message.guild.id,
-      {
+      message.guild.id, {
         level: newLevel || 0,
         xp: NewXP || 0,
         points: NewPoints || 0,

e83614fe3d8c43a0aa44289c68f369436bb74abe
José Ortiz <me@mdcdev.me>
2023-11-29 00:56:22 -0500

    refactor: Remove unused import

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const { MONGODB_URL } = process.env;
 if(MONGODB_URL === undefined) throw new Error("MONGODB_URL is not defined");
-import path from "node:path";
 
 // Connect to MongoDB
 mongoose.set('strictQuery', false);

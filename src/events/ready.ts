8c178c0071a33d4c2b0249769310a85a08072cc3
MDCDEV <me@mdcdev.me>
2023-12-24 16:26:06 -0500

    feat: Cache all members of all guilds.
    This is for stats purpose and database cache purpose.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,11 @@
-import { Events, ActivityType, ActivitiesOptions } from "discord.js";
+import { Events, ActivityType, ActivitiesOptions, Guild } from "discord.js";
 import { Bot } from "src/client";
 
 export = {
     name: "ready",
     once: true,
     run: async (client: Bot) => {
-        const activities: ActivitiesOptions[] = [{
-            name: `>help`,
-            type: ActivityType.Listening
-        },
+        const activities: ActivitiesOptions[] = [
         {
             name: "for you",
             type: ActivityType.Listening
@@ -25,6 +22,10 @@ export = {
         let activity = 1;
 
         setInterval(() => {
+            activities[1] = {
+                name: `${client.prefix}help`,
+                type: ActivityType.Listening
+            }
             activities[2] = {
                 name: `${client.guilds.cache.size} servers`,
                 type: ActivityType.Watching
@@ -33,6 +34,7 @@ export = {
                 name: `with ${client.commands.size} commands`,
                 type: ActivityType.Playing
             }; // Update command count
+
 
             client.user?.setPresence({
                 activities: [
@@ -45,13 +47,19 @@ export = {
 
         //For every server check the users
         for await (const guild of client.guilds.cache) {
-            await guild[1].members.fetch();
-            client.logger.info(`Loaded ${guild[1].members.cache.size} members from ${guild[1].name}`);
+            let members = await guild[1].members.fetch()
+            client.usersCount += members.size;
+            client.logger.info(`Loaded ${members.size} members from ${guild[1].name}`);
+            if(!await client.database.settings.selectRow(guild[1].id)){
+                client.logger.warn(`No data found for ${guild[1].name}`); //If no data found, create it.
+                await client.database.settings.insertRow(guild[1].id, guild[1].name, guild[1].systemChannelId || "", "", "", "", "", "", "", "", "", "")
+                client.logger.info(`Data inserted for ${guild[1].name}`);
+            }
         }
 
         client.logger.info(`Logged in as ${client.user?.tag}!`);
         client.logger.info(
-            `Ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
+            `Ready to serve ${client.usersCount} users in ${client.guilds.cache.size} servers.`
         );
     }
 }

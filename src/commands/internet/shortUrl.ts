3f5b897874ff80a01e12598cd5f21bc1ae98bed6
MDCDEV <me@mdcdev.me>
2023-12-24 16:26:38 -0500

    feat: New shortURL

--- src/commands/internet/shortUrl.ts
+++ src/commands/internet/shortUrl.ts
@@ -1,5 +1,5 @@
 import { CommandTypes, CommandOptions } from '../../utils/utils';
-import { EmbedBuilder } from 'discord.js';
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 import axios from 'axios';
 
 let command: CommandOptions = {
@@ -8,32 +8,50 @@ let command: CommandOptions = {
     type: CommandTypes.Internet,
     usage: "shorturl [url]",
     examples: ["shorturl https://google.com", "shorturl https://discord.gg/xXxXxXxXxX"],
-    cooldown: 30,
-    premiumCooldown: 10,
+    cooldown: 120,
+    premiumCooldown: 60,
     async run(message, args, client, language) {
         const lang = client.language.get(language || "en")?.get("shorturl") || client.language.get("en")?.get("shorturl");
         const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)([a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
-        if(!args[0]) return client.utils.sendErrorEmbed(client, language, message, this, client.utils.CommandsErrorTypes.InvalidArgument, lang.errors.noUrl);
-        if(!urlRegex.test(args[0])) return client.utils.sendErrorEmbed(client, language, message, this, client.utils.CommandsErrorTypes.InvalidArgument, lang.errors.invalidUrl);
-        const shortUrl = await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`).then((res) => res.data).catch((err) => err);
+        if (!args[0]) return client.utils.sendErrorEmbed(client, language, message, this, client.utils.CommandsErrorTypes.InvalidArgument, lang.errors.noUrl);
+        if (!urlRegex.test(args[0])) return client.utils.sendErrorEmbed(client, language, message, this, client.utils.CommandsErrorTypes.InvalidArgument, lang.errors.invalidUrl);
+        const shortUrl = await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`).then((res) => res.data).catch((err) => undefined);
+
+        console.log(shortUrl)
+
+        if(!shortUrl) return client.utils.sendErrorEmbed(client, language, message, this, client.utils.CommandsErrorTypes.InvalidArgument, lang.errors.invalidUrl);
 
         const embed = new EmbedBuilder()
-        .setTitle(lang.embed.title)
-        .addFields({
-            name: lang.embed.fields[0].name,
-            value: `\`\`\`\n${args[0]}\`\`\``
-        }, {
-            name: lang.embed.fields[1].name,
-            value: `\`\`\`\n${shortUrl}\`\`\``
-        })
-        .setTimestamp()
-        .setColor(message?.member?.displayHexColor || message.guild?.members.me?.displayHexColor || 'Random')
-        .setFooter({
-            text: message.author.tag,
-            iconURL: message.author.displayAvatarURL()
-        })
+            .setTitle(lang?.embed?.title)
+            .addFields({
+                name: lang?.embed?.fields[0]?.name,
+                value: `\`\`\`\n${args[0]}\`\`\``
+            }, {
+                name: lang?.embed?.fields[1]?.name,
+                value: `\`\`\`\n${shortUrl}\`\`\``
+            })
+            .setTimestamp()
+            .setColor(message.member?.displayHexColor || message.guild?.members.me?.displayHexColor || 'Random')
+            .setFooter({
+                text: message.author.tag,
+                iconURL: message.author.displayAvatarURL()
+            })
 
-        return message.channel.send({ embeds: [embed] });
+        const row = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel(lang?.buttons?.originalLink)
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(args[0])
+                    .setEmoji("🔗"),
+                new ButtonBuilder()
+                    .setLabel(lang?.buttons?.shortLink)
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(shortUrl)
+                    .setEmoji("🔗"),
+            );
+
+        return message.channel.send({ embeds: [embed], components: [row] })
 
     }
 
